9aedb168fdf95af739ceeea2714dd9152e2b96c9
mshr0969 <mshr60531@gmail.com>
2024-11-25 06:35:02 +0900

    display failed event

--- src/histo/pileup.cpp
+++ src/histo/pileup.cpp
@@ -6,6 +6,7 @@
 #include <TLine.h>
 #include <TLegend.h>
 #include <TStyle.h>
+#include <TLatex.h>
 
 using namespace std;
 
@@ -22,19 +23,19 @@ void pileup() {
     vector<double> *id_trk_pt = nullptr;
     vector<float> *id_trk_z0 = nullptr;
     vector<int> *vxp_type = nullptr;
-    vector<float> *vxp_z = nullptr;
+    vector<float> *true_vxp_z = nullptr;
 
     tree->SetBranchAddress("id_trk_pt", &id_trk_pt);
     tree->SetBranchAddress("id_trk_z0", &id_trk_z0);
     tree->SetBranchAddress("vxp_type", &vxp_type);
-    tree->SetBranchAddress("vxp_z", &vxp_z);
+    tree->SetBranchAddress("true_vxp_z", &true_vxp_z);
 
     int entries =  tree->GetEntries();
     cout << "Total number of events: " << entries << endl;
 
-    tree->GetEntry(33);
-    string title =  "z_{0} weighted by p_{T}";
-    TH1D *h1 = new TH1D("h1", title.c_str(), 8, -300, 300);
+    tree->GetEntry(4989);
+    string title =  "";
+    TH1D *h1 = new TH1D("h1", title.c_str(), 256, -200, 200);
     h1->SetFillColor(kBlue - 10);
 
     std::map<int, double> binValues;
@@ -54,7 +55,7 @@ void pileup() {
     h1->Draw();
 
     TLegend *legend = new TLegend(0.72, 0.72, 0.89, 0.89);
-    legend->AddEntry(h1, "ID Track", "f");
+    legend->AddEntry(h1, "Sum of  track p_{T}", "f");
 
     gPad->Update();
 
@@ -67,17 +68,28 @@ void pileup() {
     dummy_pv->SetLineWidth(2);
     dummy_pu->SetLineStyle(2);
 
-    for (int i = 0; i < vxp_z->size(); i++) {
-        if (vxp_type->at(i) == 1) {
-            double z = vxp_z->at(i);
-            TLine *line = new TLine(z, 0, z, ymax);
-            line->SetLineColor(kRed);
-            line->SetLineWidth(2);
-            line->Draw();
-        }
+    // true_vxp_zの、初めから100個のうち、最も多いものをprimary vertexとする
+    double primary_vertex;
+    vector<double> truth_z;
+    for (size_t i = 0; i < 100; ++i) {
+        truth_z.push_back(true_vxp_z->at(i));
     }
+    primary_vertex = *max_element(truth_z.begin(), truth_z.end());
+    TLine *line_pv = new TLine(primary_vertex, 0, primary_vertex, ymax);
+    line_pv->SetLineColor(kRed);
+    line_pv->SetLineWidth(2);
+    line_pv->Draw();
 
-    legend->AddEntry(dummy_pv, "Reco PV", "l");
+    TLatex latex;
+    latex.SetNDC();
+    latex.SetTextFont(72); // ATLASフォントスタイル
+    latex.SetTextSize(0.04);
+    latex.DrawLatex(0.13, 0.8, "ATLAS");
+
+    latex.SetTextFont(42); // 標準のフォントスタイルに戻す
+    latex.DrawLatex(0.22, 0.8, "Simulation Work In Progress");
+    latex.DrawLatex(0.13, 0.75, "#sqrt{s} = 14 TeV");
+    legend->AddEntry(dummy_pv, "True PV", "l");
     legend->SetBorderSize(0);
     legend->Draw();
 
